3997237dd9948d2d6f6b62afca2f5e5dcd1e8868
Bolwl <80219386+Bolwl@users.noreply.github.com>
2022-05-01 15:22:39 +0300

    Update server_data.php

--- growtopia/server_data.php
+++ growtopia/server_data.php
@@ -1,7 +1,7 @@
 server|213.179.209.168
 port|17257
 type|1
-#maint|Server is under maintenance. We will be back online shortly. Thank you for your patience!
+maint|test
 beta_server|beta.growtopiagame.com
 beta_port|26999
 beta_type|1
