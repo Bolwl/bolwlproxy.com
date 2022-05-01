1f3b5f6a9c686f02b9d3f1136ecb7fd59748165b
Bolwl <80219386+Bolwl@users.noreply.github.com>
2022-05-01 15:15:42 +0300

    Create server_data.php

---
+++ growtopia/server_data.php
@@ -0,0 +1,13 @@
+server|213.179.209.168
+port|17257
+type|1
+#maint|Server is under maintenance. We will be back online shortly. Thank you for your patience!
+beta_server|beta.growtopiagame.com
+beta_port|26999
+beta_type|1
+beta2_server|beta2.growtopiagame.com
+beta2_port|26999
+beta2_type|1
+type2|1
+meta|TLddQ2jYAo
+RTENDMARKERBS1001
